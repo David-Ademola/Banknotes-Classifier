d7c91ee3863f5170060421936ca88ebf4ce6d08d
David-Ademola <dakinwande350@gmail.com>
2023-06-10 15:12:46 +0100

    Initial Commit

--- banknotes.py
+++ banknotes.py
@@ -2,9 +2,7 @@ import csv
 import os
 
 from sklearn.model_selection import train_test_split
-# from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import Perceptron
-# from sklearn.naive_bayes import GaussianNB
 
 model = Perceptron()
 
